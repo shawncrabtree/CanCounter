63e71c5d2649e35dcba5aab8b0ab75834f4207f1
cuiltyt <cuiltyt@onid.orst.edu>
2014-11-18 18:29:57 -0800

    simple inventory view

--- cs361/foodbank/urls.py
+++ cs361/foodbank/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import patterns, url
 
 from foodbank import views
+from foodbank.views import Inventory
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
+    url(r'^inventory/', Inventory.as_view(), name='item_list'),
 )
--- cs361/foodbank/views.py
+++ cs361/foodbank/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic.list import ListView
+
+from foodbank.models import Item
 
 def index(request):
 	return HttpResponse("Hello, world")
+
+class Inventory(ListView):
+	model = Item

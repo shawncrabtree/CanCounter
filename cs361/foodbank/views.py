7ddd139eb44318413535e21f1cdae777f7e62c3c
Taylor Cuilty <taylorcuilty@10-196-19-248.wireless.oregonstate.edu>
2014-12-02 18:03:37 -0800

    login directs to inventory page

--- cs361/foodbank/views.py
+++ cs361/foodbank/views.py
@@ -8,13 +8,6 @@ from foodbank.models import Item
 
 def login(request):
 	return render(request,"foodbank/login.html")
-	
-def hello(request):
-	user = User.objects.filter(username=request.GET['username'])
-	if check_password(request.GET['password'], user[0].password):
-		return HttpResponse("Welcome!")
-	else:
-		return HttpResponse("Incorrect username or password")
 
 class Inventory(ListView):
 	model = Item

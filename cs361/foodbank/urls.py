3d1960af0de0a113f0209ba868fb23e2583dd025
Taylor Cuilty <taylorcuilty@10-196-19-248.wireless.oregonstate.edu>
2014-12-02 18:19:07 -0800

    user can add monetary donation

--- cs361/foodbank/urls.py
+++ cs361/foodbank/urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls import patterns, url
 
 from foodbank import views
-from foodbank.views import Inventory
+from foodbank.views import Inventory, DonationCreate
 
 urlpatterns = patterns('',
     url(r'^login/', views.login, name='login'),
     url(r'^inventory/', Inventory.as_view(), name='item_list'),
-    url(r'^hello/', views.hello, name='hello'),
+    url(r'^create/', DonationCreate.as_view(), name='donation_form'),
 )
--- cs361/foodbank/views.py
+++ cs361/foodbank/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView
 from django.contrib.auth.models import User
 from django.contrib.auth.hashers import check_password
 
-from foodbank.models import Item
+from foodbank.models import Item, Donation
 
 def login(request):
 	return render(request,"foodbank/login.html")
 
 class Inventory(ListView):
 	model = Item
+
+class DonationCreate(CreateView):
+    model = Donation
